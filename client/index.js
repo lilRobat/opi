748eb1e66cac492b8590f0d4fedb8e3f95827113
Robert Kommeter <kommeterrobert@gmail.com>
2022-07-31 15:58:22 +0200

    Create POST route "interpret"
    Client can now send the code to the server.

--- client/index.js
+++ client/index.js
@@ -1,3 +1,12 @@
 function submitCode() {
-    console.log("Hello World")
+    let pythonCode = document.getElementById("editorText").value
+    let data = { text: pythonCode };
+
+    fetch("http://127.0.0.1:8000/interpret/", {
+        method: "POST",
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify(data)
+    }).then(res => {
+        console.log("Request complete! response:", res);
+    });
 }
--- server/main.py
+++ server/main.py
@@ -1,8 +1,29 @@
 from fastapi import FastAPI
+from typing import Union
+from pydantic import BaseModel
+from fastapi.middleware.cors import CORSMiddleware
+
+class Code(BaseModel):
+    text: str
 
 app = FastAPI()
 
+origins = [
+    "http://127.0.0.1",
+    "http://127.0.0.1:5500",
+]
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
+
+@app.post("/interpret/")
+async def interpret_code(code: Code):
+    return code
